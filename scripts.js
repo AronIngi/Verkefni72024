47e2333730b4fdb239926a0fd9bf6493a487dedf
Clifford <cliff@Kratos.Cliff>
2024-10-14 17:16:09 +0000

    skil

--- scripts.js
+++ scripts.js
@@ -49,32 +49,138 @@ function split(str, separator = ' ') {
 // Grunnföll sem skilgreina á
 
 function longest(str) {
-  // Útfæra
+  if(isString(str))
+  {
+    var longest_word = "";
+    words = str.split(' ');
+    for(const word of words)
+    {
+      if(word.length > longest_word.length)
+      {
+        longest_word = word;
+      }
+    }
+    return longest_word;
+  }
+  return null;
 }
+console.assert(longest("a ab") === "ab");
+console.assert(longest("") === "");
+console.assert(longest(null) === null);
 
 function shortest(str) {
-  // Útfæra
+  if(isString(str))
+  {
+    var shortest_word = str;
+    words = str.split(' ');
+    for(const word of words)
+    {
+      if(word.length < shortest_word.length)
+      {
+        shortest_word = word;
+      }
+    }
+    return shortest_word;
+  }
+  return null;
 }
+console.assert(shortest("a ab") === "a");
+console.assert(shortest("") === "");
+console.assert(shortest(null) === null);
 
 function reverse(str) {
-  // Útfæra
+  if(isString(str))
+  {
+    var words = str.split('');
+    var reverse = str.split('');
+    var j = words.length-1;
+    var i = 0;
+    while(j >= 0)
+    {
+      reverse[i] = words[j];
+      i++;
+      j--;
+    }
+    return reverse.join("");
+  }
+  return null;
 }
+console.assert(reverse("abc") === "cba");
+console.assert(reverse("") === "");
+console.assert(reverse(null) === null);
 
 function palindrome(str) {
-  // Útfæra
+  if(isString(str))
+  {
+    if(str === "")
+      return false;
+    else if(str === reverse(str))
+      return true;
+  }
+  return false;
 }
+console.assert(palindrome("racecar") === true, "1");
+console.assert(palindrome("") === false, "2");
+console.assert(palindrome(null) === false, "3");
 
 function vowels(str) {
-  // Útfæra
+  if(isString(str))
+  {
+    const words = str.split('');
+    var count = 0;
+    for(const word of words)
+    {
+      if(VOWELS.includes(word))
+      {
+        count++;
+      }
+
+    }
+    return count;
+  }
+  return 0;
 }
+console.assert(vowels("aba") === 2, "1");
+console.assert(vowels("") === 0, "2");
+console.assert(vowels(null) === 0, "3");
 
 function consonants(str) {
-  // Útfæra
+  if(isString(str))
+  {
+    const words = str.split('');
+    var count = 0;
+    for(const word of words)
+    {
+      if(CONSONANTS.includes(word))
+      {
+        count++;
+      }
+
+    }
+    return count;
+  }
+  return 0;
 }
+console.assert(consonants("aba") === 1, "1");
+console.assert(consonants("") === 0, "2");
+console.assert(consonants(null) === 0, "3");
 
 //------------------------------------------------------------------------------
 // Leiðbeint ferli
 
 function start() {
-  // Útfæra
+  alert("Ýttu á OK og skrifaðu inn streng í texta boxið sem kemur upp");
+  var input = prompt();
+  alert("longest: " + longest(input)
+        + "\n"
+        + "shortest: " + shortest(input)
+        + "\n"
+        + "reverse: " + reverse(input)
+        + "\n"
+        + "palindrome: " + palindrome(input)
+        + "\n"
+        + "vowels: " + vowels(input)
+        + "\n"
+        + "consonants: " + consonants(input)
+  );
 }
